32014b952103d8ad8d89a9ccdb002bda0e502fb6
Beatriz <byordaky@gmail.com>
2024-01-29 12:45:55 -0300

    create url to access image

--- src/main/java/io/github/beatrizyordaky/spaceimagesapi/domain/service/ImageService.java
+++ src/main/java/io/github/beatrizyordaky/spaceimagesapi/domain/service/ImageService.java
@@ -2,6 +2,10 @@ package io.github.beatrizyordaky.spaceimagesapi.domain.service;
 
 import io.github.beatrizyordaky.spaceimagesapi.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
     Image save(Image image);
+
+    Optional<Image> getById(String id);
 }
