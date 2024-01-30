bcac267a1716f0f3130a84997931de4e8c873895
Beatriz <byordaky@gmail.com>
2024-01-30 18:33:00 -0300

    add dto

--- src/main/java/io/github/beatrizyordaky/spaceimagesapi/application/images/ImageDTO.java
+++ src/main/java/io/github/beatrizyordaky/spaceimagesapi/application/images/ImageDTO.java
@@ -1,2 +1,16 @@
-package io.github.beatrizyordaky.spaceimagesapi.application.images;public class ImageDTO {
+package io.github.beatrizyordaky.spaceimagesapi.application.images;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class ImageDTO {
+    private String url;
+    private String name;
+    private String extension;
+    private Long size;
+    private LocalDate uploadDate;
 }
